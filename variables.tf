2e0ead66b8ffa9dc65708e32e2cb537a3cedf0b5
Kyle Alderson <kyle.alderson91@gmail.com>
2022-03-02 16:45:43 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,13 @@
+##########################
+### General variables ####   
+variable "project_id" {
+  type        = string
+  description = "ID of the project."
+}
 
+##########################
+### Bucket variables ####   
+variable "name" {
+  type        = string
+  description = "name of storage bucket"
+}
