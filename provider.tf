cb4afe35e44a4782daade81791136b2c701bafaa
kyalders <kyle.alderson91@gmail.com>
2022-03-02 22:12:31 +0000

    updated tf code

--- provider.tf
+++ provider.tf
@@ -1,1 +1,25 @@
+terraform {
+  required_version = "~>1.1.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.0.0, < 5.0.0"
+    }
 
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 3.0.0, < 5.0.0"
+    }
+  }
+
+}
+
+
+
+provider "google" {
+  project = var.project_id
+}
+
+provider "google-beta" {
+  project = var.project_id
+}
