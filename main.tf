1aa7c5e9f670b4bc4a80e956aafcab5abb6ed0b0
Kyle Alderson <kyle.alderson91@gmail.com>
2022-03-03 07:24:38 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
-resource "google_kms_key_ring" "keyring" {
-  name     = "keyring-test"
-  location = "global"
-  project = var.project_id
+// Terraform plugin for creating random ids
+resource "random_id" "instance_id" {
+ byte_length = 8
+}
+
+// A single Compute Engine instance
+resource "google_compute_instance" "default" {
+ name         = "flask-vm-${random_id.instance_id.hex}"
+ machine_type = "f1-micro"
+ zone         = "us-west1-a"
+
+ boot_disk {
+   initialize_params {
+     image = "debian-cloud/debian-9"
+   }
+ }
 }
