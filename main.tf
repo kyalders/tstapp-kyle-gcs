baaec70d54306df4d1059cc969d13adbd2cc0a50
Kyle Alderson <kyle.alderson91@gmail.com>
2022-03-08 09:21:44 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "google_compute_instance" "http_server" {
   zone         = "us-east4-b"
   name         = "${var.env}-apache2-instance"
   machine_type = "f1-micro"
+  allow_stopping_for_update = true
 
   metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<html><body><h1>Environment: ${var.env}</h1></body></html>' | sudo tee /var/www/html/index.html"
 
