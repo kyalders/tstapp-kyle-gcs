9380661d54831f4dc1eb08ae18bc9f5e0628b1b4
kyalders <kyle.alderson91@gmail.com>
2022-04-23 20:16:24 -0500

    testing dns

--- composer.tf
+++ composer.tf
@@ -2,8 +2,8 @@ module "composer_service_account" {
   source     = "terraform-google-modules/service-accounts/google"
   version    = "~> 3.0"
   project_id = var.project_id
-  prefix     = "composer-sa"
-  names      = ["first", "second"]
+  prefix     = "${var.env}-"
+  names      = ["composer-sa"]
   project_roles = ["${var.project_id}=>roles/composer.worker",
   "${var.project_id}=>roles/iam.serviceAccountUser"]
 }
@@ -34,4 +34,8 @@ resource "google_dns_record_set" "artifact-registry-cname" {
   ttl          = 300
 
   rrdatas = ["pkg.dev."]
+
+  depends_on = [
+    resource.google_dns_managed_zone.artifact-registry-zone
+  ]
 }
